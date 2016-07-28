a49cf175c77c4c0ceb3c0f16543985afbf89031b
Colin Johnston <dev@electronspin.com>
2016-07-28 14:53:15 -0700

    have only saffo website and solano concepts as recent projects

--- includes/nav-projects.php
+++ includes/nav-projects.php
@@ -1,10 +1,10 @@
 <li class="nav-list-title">Recent projects</li>
         <li><a href="/projects/paul-saffo-website">Paul Saffo Website</a></li>
         <li><a href="/projects/solanolabs-ci-concepts"><?php echo $slcc ?></a></li>
-        <li><a href="/projects/moovweb-dev-center"><?php echo $mwdc ?></a></li>
-        <li><a href="/projects/moovweb-control-center"><?php echo $mwcc ?></a></li>
         
         <li class="nav-list-title">More projects</li>
+        <li><a href="/projects/moovweb-dev-center"><?php echo $mwdc ?></a></li>
+        <li><a href="/projects/moovweb-control-center"><?php echo $mwcc ?></a></li>
         <li><a href="/projects/libsass-com"><?php echo $libsass ?></a></li>
         <li><a href="/projects/moovweb-dashboard"><?php echo $mwsdk ?></a></li>
         <li><a href="/projects/the-real-sf-tour-website">The Real SF Tour Website</a></li>
