55af1a96117ab1bd2de0fbfc08ccc911ab0e01fe
Colin Johnston <dev@electronspin.com>
2016-07-28 15:06:13 -0700

    fix modal

--- projects/paul-saffo-website/modals.php
+++ projects/paul-saffo-website/modals.php
@@ -1,5 +1,7 @@
-<div id="saffo-homepage" class="reveal-modal expand modal modal-img-border"> <!-- [expand, xlarge, large, medium, small] -->
-  <h2>Paul Saffo Homepage</h2>
-  <img src="/images/saffo-homepage_full.jpg" alt="Paul Saffo Homepage">
-  <a class="close-reveal-modal">&#215;</a>
+<div id="saffo-home-responsive" class="reveal-modal expand modal modal-img-border">
+    <!-- [expand, xlarge, large, medium, small] -->
+    <h2>Paul Saffo Homepage</h2>
+    <img src="/images/saffo-homepage_full.jpg" alt="Paul Saffo Homepage">
+    <p><a href="http://www.saffo.com/" name="" target="_blank">www.saffo.com</a></p>
+    <a class="close-reveal-modal">&#215;</a>
 </div>
