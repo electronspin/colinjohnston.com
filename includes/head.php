4fefd3ec06662dea348bdb823395dea9b09d1656
Colin Johnston <dev@electronspin.com>
2018-08-21 13:04:57 -0700

    fix js errors in custom - some refactoring too

--- includes/head.php
+++ includes/head.php
@@ -6,10 +6,10 @@
   <link href="/favicon.ico" rel="icon" type="image/x-icon"/>
   <link rel="stylesheet" href="/stylesheets/css/app.min.css">
   
-  <!-- <script src="/javascripts/modernizr.foundation.js" async></script> -->
-  <script src="//cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
-  <!-- <script src="/javascripts/picturefill.min.js" async></script> -->
-  <script src="//cdnjs.cloudflare.com/ajax/libs/picturefill/2.3.1/picturefill.min.js"></script>
+  <script src="/javascripts/modernizr.foundation.js" async></script>
+  <!-- <script src="//cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script> -->
+  <script src="/javascripts/picturefill.min.js" async></script>
+  <!-- <script src="//cdnjs.cloudflare.com/ajax/libs/picturefill/2.3.1/picturefill.min.js"></script> -->
   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
   <script src="/javascripts/app.min.js"></script>
 
