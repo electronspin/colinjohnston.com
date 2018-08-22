4dc851309dc345ff963f6bd9999ee39f89f420a9
Colin Johnston <dev@electronspin.com>
2018-08-21 18:07:27 -0700

    include analytics only if in production

--- index.php
+++ index.php
@@ -12,17 +12,68 @@
         <div id="page" class="row">
             <?php include($_SERVER['DOCUMENT_ROOT'].'/includes/sidebar-home.php') ?>
             <div id="content" class="eight columns project-thumb-grid">
-                <!-- Convert this to flexbox! It's 2015. -->
+                <!-- Convert this to CSS Grid! It's 2018. -->
                 <ul class="block-grid two-up mobile">
+
+                    <li>
+                        <a href="/projects/rollbar-account-dashboard">
+                            <div class="imagebox">
+                                <figure>
+                                    <img srcset="images/placeholder_small.jpg 400w,
+images/placeholder_medium.jpg 800w,
+images/placeholder_large.jpg 1200w" sizes="(min-width: 48em) 33.3vw, 100vw" src="images/placeholder_small.jpg"
+                                        alt="<?php echo $rbad; ?>">
+                                    <figcaption>
+                                        <?php echo $rbad; ?>
+                                    </figcaption>
+                                </figure>
+                            </div>
+                        </a>
+                    </li>
+                    
+                    <li>
+                        <a href="/projects/rollbar-marketing-website">
+                            <div class="imagebox">
+                                <figure>
+                                    <img srcset="images/placeholder_small.jpg 400w,
+images/placeholder_medium.jpg 800w,
+images/placeholder_large.jpg 1200w" sizes="(min-width: 48em) 33.3vw, 100vw" src="images/placeholder_small.jpg"
+                                        alt="<?php echo $rbws; ?>">
+                                    <figcaption>
+                                        <?php echo $rbws; ?>
+                                    </figcaption>
+                                </figure>
+                            </div>
+                        </a>
+                    </li>
+                    
+                    <li>
+                        <a href="/projects/rollbar-identity-refresh">
+                            <div class="imagebox">
+                                <figure>
+                                    <img srcset="images/placeholder_small.jpg 400w,
+images/placeholder_medium.jpg 800w,
+images/placeholder_large.jpg 1200w" sizes="(min-width: 48em) 33.3vw, 100vw" src="images/placeholder_small.jpg"
+                                        alt="<?php echo $rbid; ?>">
+                                    <figcaption>
+                                        <?php echo $rbid; ?>
+                                    </figcaption>
+                                </figure>
+                            </div>
+                        </a>
+                    </li>
                     
                     <li>
                         <a href="/projects/solanolabs-ci-session-view">
                             <div class="imagebox">
                                 <figure>
                                     <img srcset="images/solanolabs-sessionview-v2_small.jpg 400w,
-                        images/solanolabs-sessionview-v2_medium.jpg 800w,
-                        images/solanolabs-sessionview-v2_large.jpg 1200w" sizes="(min-width: 48em) 33.3vw, 100vw" src="images/solanolabs-sessionview-v2_small.jpg" alt="Paul Saffo Website">
-                                    <figcaption><?php echo $slcs; ?></figcaption>
+images/solanolabs-sessionview-v2_medium.jpg 800w,
+images/solanolabs-sessionview-v2_large.jpg 1200w" sizes="(min-width: 48em) 33.3vw, 100vw" src="images/solanolabs-sessionview-v2_small.jpg"
+                                        alt="<?php echo $slcs; ?>">
+                                    <figcaption>
+                                        <?php echo $slcs; ?>
+                                    </figcaption>
                                 </figure>
                             </div>
                         </a>
@@ -32,35 +83,23 @@
                             <div class="imagebox">
                                 <figure>
                                     <img srcset="images/saffo-home-responsive_small.jpg 400w,
-                        images/saffo-home-responsive_medium.jpg 800w,
-                        images/saffo-home-responsive_large.jpg 1200w" sizes="(min-width: 48em) 33.3vw, 100vw" src="images/saffo-home-responsive_small.jpg" alt="Paul Saffo Website">
+images/saffo-home-responsive_medium.jpg 800w,
+images/saffo-home-responsive_large.jpg 1200w" sizes="(min-width: 48em) 33.3vw, 100vw" src="images/saffo-home-responsive_small.jpg"
+                                        alt="Paul Saffo Website">
                                     <figcaption>Paul Saffo Website</figcaption>
                                 </figure>
                             </div>
                         </a>
                     </li>
-                    
-                    <!-- <li>
-                        <a href="/projects/solanolabs-ci-concepts">
-                            <div class="imagebox">
-                                <figure>
-                                    <img srcset="images/solanolabs-ci-concept-main-passed_small.jpg 400w,
-                        images/solanolabs-ci-concept-main-passed_medium.jpg 800w,
-                        images/solanolabs-ci-concept-main-passed_large.jpg 1200w" sizes="(min-width: 48em) 33.3vw, 100vw" src="images/solanolabs-ci-concept-main-passed_small.jpg" alt="<?php echo $slcc ?>">
-                                    <figcaption>
-                                        <?php echo $slcc ?>
-                                    </figcaption>
-                                </figure>
-                            </div>
-                        </a>
-                    </li> -->
+
                     <li>
                         <a href="/projects/moovweb-dev-center">
                             <div class="imagebox">
                                 <figure>
                                     <img srcset="images/moovweb-dev-center-downloads_small.jpg 400w,
-                        images/moovweb-dev-center-downloads_medium.jpg 800w,
-                        images/moovweb-dev-center-downloads_large.jpg 1200w" sizes="(min-width: 48em) 33.3vw, 100vw" src="images/moovweb-dev-center-downloads_small.jpg" alt="<?php echo $mwdc ?>">
+images/moovweb-dev-center-downloads_medium.jpg 800w,
+images/moovweb-dev-center-downloads_large.jpg 1200w" sizes="(min-width: 48em) 33.3vw, 100vw" src="images/moovweb-dev-center-downloads_small.jpg"
+                                        alt="<?php echo $mwdc ?>">
                                     <figcaption>
                                         <?php echo $mwdc ?>
                                     </figcaption>
@@ -73,63 +112,12 @@
                             <div class="imagebox">
                                 <figure>
                                     <img srcset="images/moovweb-control-center-main_small.jpg 400w,
-                        images/moovweb-control-center-main_medium.jpg 800w,
-                        images/moovweb-control-center-main_large.jpg 1200w" sizes="(min-width: 48em) 33.3vw, 100vw" src="images/moovweb-control-center-main_small.jpg" alt="<?php echo $mwcc ?>">
+images/moovweb-control-center-main_medium.jpg 800w,
+images/moovweb-control-center-main_large.jpg 1200w" sizes="(min-width: 48em) 33.3vw, 100vw" src="images/moovweb-control-center-main_small.jpg"
+                                        alt="<?php echo $mwcc ?>">
                                     <figcaption>
                                         <?php echo $mwcc ?>
                                     </figcaption>
-                                </figure>
-                            </div>
-                        </a>
-                    </li>
-                    <!-- <li>
-                        <a href="/projects/moovweb-dashboard">
-                            <div class="imagebox">
-                                <figure>
-                                    <img srcset="images/moovweb-sdk-dashboard_small.jpg 400w,
-                        images/moovweb-sdk-dashboard_medium.jpg 800w,
-                        images/moovweb-sdk-dashboard_large.jpg 1200w" sizes="(min-width: 48em) 33.3vw, 100vw" src="images/moovweb-sdk-dashboard_small.jpg" alt="<?php echo $mwsdk ?>">
-                                    <figcaption>
-                                        <?php echo $mwsdk ?>
-                                    </figcaption>
-                                </figure>
-                            </div>
-                        </a>
-                    </li> -->
-                    <!-- <li>
-                        <a href="/projects/libsass-com">
-                            <div class="imagebox">
-                                <figure>
-                                    <img srcset="images/libsass-com_small.jpg 400w,
-                        images/libsass-com_medium.jpg 800w,
-                        images/libsass-com_large.jpg 1200w" sizes="(min-width: 48em) 33.3vw, 100vw" src="images/libsass-com_small.jpg" alt="<?php echo $libsass ?>">
-                                    <figcaption>
-                                        <?php echo $libsass ?>
-                                    </figcaption>
-                                </figure>
-                            </div>
-                        </a>
-                    </li> -->
-                    <li>
-                        <a href="/projects/the-real-sf-tour-website">
-                            <div class="imagebox">
-                                <figure>
-                                    <img srcset="images/therealsftour-home_small.jpg 400w,
-                        images/therealsftour-home_medium.jpg 800w,
-                        images/therealsftour-home_large.jpg 1200w" sizes="(min-width: 48em) 33.3vw, 100vw" src="images/therealsftour-home_small.jpg" alt="" />
-                        <figcaption>The Real SF Tour Website</figcaption>
-                                </figure>
-                            </div>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="/projects/orphic-gallery-website">
-                            <div class="imagebox">
-                                <figure>
-                                    <img srcset="images/orphic-gallery_small.jpg 400w,
-                        images/orphic-gallery_medium.jpg 800w,
-                        images/orphic-gallery_large.jpg 1200w" sizes="(min-width: 48em) 33.3vw, 100vw" src="images/orphic-gallery_small.jpg" alt="" />
-                                    <figcaption>Orphic Gallery Website</figcaption>
                                 </figure>
                             </div>
                         </a>
