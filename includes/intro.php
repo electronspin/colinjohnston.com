8a3916f148435c55e35122b3db3ce742f2aa55fe
Colin Johnston <dev@electronspin.com>
2018-08-28 09:31:15 -0700

    minor tweaks

--- includes/intro.php
+++ includes/intro.php
@@ -1,1 +1,1 @@
-Digital Product Designer and Graphic Designer in San Francisco, California. I design for web and mobile, with a focus on user-centered design process and clear, thoughtful user experiences.
+Digital Product Designer and Graphic Designer in San Francisco, California.<br/>I design for web and mobile, with a focus on user-centered design process and clear, thoughtful user experiences.
