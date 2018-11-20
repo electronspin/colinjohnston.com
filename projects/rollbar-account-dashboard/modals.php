3ac787510e4a4b69e1e344ab5fac5422a86daa5f
Colin Johnston <dev@electronspin.com>
2018-11-19 16:59:28 -0800

    more modals refactor

--- projects/rollbar-account-dashboard/modals.php
+++ projects/rollbar-account-dashboard/modals.php
@@ -1,11 +1,9 @@
-<!-- [expand, xlarge, large, medium, small] -->
-
 <!-- rollbar-account-dashboard-cover -->
 <div id="rollbar-account-dashboard-cover" class="reveal-modal full modal-img-border"> 
     <!-- <h2><?php //echo $rbad; ?></h2> -->
     <div class="modal-container">
       <!-- can be carousel -->
-    <img src="/images/rollbar-account-dashboard-cover_full.jpg" alt="<?php echo $rbad; ?>">
+      <img src="/images/rollbar-account-dashboard-cover_full.jpg" alt="<?php echo $rbad; ?>">
     </div>
     <a class="close-reveal-modal">&#215;</a>
 </div>
