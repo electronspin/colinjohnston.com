9812c2cb7b98fe0dc873dcc77363471a33eacd36
Colin Johnston <neoterica@gmail.com>
2019-06-29 10:42:38 -0700

    make intro compnent a div

--- includes/intro.php
+++ includes/intro.php
@@ -1,6 +1,10 @@
-<p class="c-intro">
-I'm a Designer based in San Francisco, California.  
-</p>
-<p class="c-intro">
-I help companies deliver quality products through research and design sprints, user-centered interaction design, rapid prototyping, and polished front-end design. I thrive on iterating with cross-functional teams to arrive at simple solutions to complex problems.
-</p>
+<div class="c-intro">
+  <p>
+    I'm a Designer based in San Francisco, California.
+  </p>
+  <p>
+    I help companies deliver quality products through research and design sprints, user-centered interaction design,
+    rapid prototyping, and polished front-end design. I thrive on iterating with cross-functional teams to arrive at
+    simple solutions to complex problems.
+  </p>
+</div>
