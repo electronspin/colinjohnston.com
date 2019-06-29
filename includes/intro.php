9a498f84ffea08c50af01c7140d1f68ea698ce05
Colin Johnston <neoterica@gmail.com>
2019-06-28 19:22:54 -0700

    refactor intro with new c-intro component

--- includes/intro.php
+++ includes/intro.php
@@ -1,6 +1,6 @@
-<p class="intro">
+<p class="c-intro">
 I'm a Designer based in San Francisco, California.  
 </p>
-<p class="intro">
+<p class="c-intro">
 I help companies deliver quality products through research and design sprints, user-centered interaction design, rapid prototyping, and polished front-end design. I thrive on iterating with cross-functional teams to arrive at simple solutions to complex problems.
 </p>
