1340b85be8eb8fbbe5153484cee61c116cebb8b6
Colin Johnston <neoterica@gmail.com>
2019-06-28 22:52:28 -0700

    continue refactor logo to new branding component

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,28 +1,30 @@
 <div class="o-sidebar o-layout__grid-item">
 
-        <div class="c-branding">
-            <a class="c-branding__logo animate" href="/"><img class="c-logo" src="/images/colinjohnston-logo-DIN.png" alt="Colin Johnston"></a>
-            <a href="#" class="open-panel"><span class="svg-icon">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 128">
-  <path id="menu-icon" d="M0 119.5h128v-16H0v16zM0 72h128V56H0v16zm0-48h128V8H0v16z"/>
-</svg>
+    <div class="c-branding">
+        <a class="c-branding__logo animate" href="/"><img class="c-logo" src="/images/colinjohnston-logo-DIN.png"
+                alt="Colin Johnston"></a>
+        <a href="#" class="open-panel"><span class="svg-icon">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 128">
+                    <path id="menu-icon" d="M0 119.5h128v-16H0v16zM0 72h128V56H0v16zm0-48h128V8H0v16z" />
+                </svg>
             </span>Menu</a>
-        </div>
-        <div class="hide-for-small">
-            <?php include('intro.php') ?>
-        </div>
-        <nav>
-            <a href="#" class="close-panel"><span class="svg-icon">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 128">
-  <path id="close-icon" d="M64 128c35.4 0 64-28.6 64-64S99.4 0 64 0 0 28.6 0 64s28.6 64 64 64zM40 99.5L28.5 88l24-24-24-24L40 28.5l24 24 24-24L99.5 40l-24 24 24 24L88 99.5l-24-24-24 24z"/>
-</svg>
+    </div>
+    <div class="hide-for-small">
+        <?php include('intro.php') ?>
+    </div>
+    <nav>
+        <a href="#" class="close-panel"><span class="svg-icon">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 128">
+                    <path id="close-icon"
+                        d="M64 128c35.4 0 64-28.6 64-64S99.4 0 64 0 0 28.6 0 64s28.6 64 64 64zM40 99.5L28.5 88l24-24-24-24L40 28.5l24 24 24-24L99.5 40l-24 24 24 24L88 99.5l-24-24-24 24z" />
+                </svg>
             </span></a>
-            <ul class="side-nav">
+        <ul class="side-nav">
             <?php include($_SERVER['DOCUMENT_ROOT'].'/includes/nav-projects.php') ?>
-    
-            </ul>
-        </nav>
-        <p class="contact-button hide-for-small"><a class="contact-email" href="/connect-and-collaborate">Contact</a></p>
+
+        </ul>
+    </nav>
+    <p class="contact-button hide-for-small"><a class="contact-email" href="/connect-and-collaborate">Contact</a></p>
 
 </div>
 <!--end sidebar-->
