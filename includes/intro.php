122321d859235891bcb87355c9983395697d523b
Colin Johnston <neoterica@gmail.com>
2019-07-07 14:24:24 -0700

    Edits to intro text

--- includes/intro.php
+++ includes/intro.php
@@ -3,8 +3,7 @@
     I'm a Designer based in San Francisco, California.
   </p>
   <p>
-    I help companies deliver quality products through research and design sprints, user-centered interaction design,
-    rapid prototyping, and polished front-end design. I thrive on iterating with cross-functional teams to arrive at
-    simple solutions to complex problems.
+    I help deliver quality products through research and design sprints, user-centered interaction design,
+    rapid prototyping, and polished front-end design. 
   </p>
 </div>
