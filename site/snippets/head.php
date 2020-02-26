0a75eb447dd8ea880dbac901bb779045379c72c0
Colin Johnston <dev@electronspin.com>
2020-02-25 17:04:39 -0800

    updates assets paths for css js to kirby `/public`

--- site/snippets/head.php
+++ site/snippets/head.php
@@ -5,10 +5,10 @@
   <title>
     <?= $page->title() ?> | <?= $site->title() ?>
   </title>
-  <link rel="stylesheet" href="/www/stylesheets/css/main.min.css" >
-  <script src="/www/javascripts/modernizr.foundation.js"></script>
-  <script src="/www/javascripts/picturefill.min.js"></script>
+  <link rel="stylesheet" href="/assets/css/main.min.css" >
+  <script src="/assets/js/modernizr.foundation.js"></script>
+  <script src="/assets/js/picturefill.min.js"></script>
   <script src="https://code.jquery.com/jquery-1.9.1.min.js" integrity="sha256-wS9gmOZBqsqWxgIVgA8Y9WcQOa7PgSIX+rPA0VL2rbQ=" crossorigin="anonymous"></script>
-  <script src="/www/javascripts/app.min.js"></script>
+  <script src="/assets/js/app.min.js"></script>
 </head>
 <body>
