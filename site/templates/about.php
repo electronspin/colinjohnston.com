1930b85a07f3e586235e45a692fa9c9010ea4d51
Colin Johnston <dev@electronspin.com>
2020-02-27 12:01:35 -0800

    initial contact and about pages WIP

---
+++ site/templates/about.php
@@ -0,0 +1,21 @@
+<?php snippet('head') ?>
+
+    <div id="pagetop"></div>
+    <div class="o-wrapper o-wrapper-grid o-wrapper--no-header">
+
+    <?php snippet('sidebar') ?>
+
+    <main id="content" class="o-content o-layout__grid-item">
+
+    <h1><?= $page->title() ?></h1>
+
+    <?= $page->text()->kirbytext() ?>
+
+    <?php snippet('menu-main') ?>
+
+    </main>
+
+    <?php snippet('footer') ?>
+
+    </div>
+
