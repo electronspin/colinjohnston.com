25d492d4bd362bad2958762e3527b4ddb591d121
Colin Johnston <dev@electronspin.com>
2020-02-27 11:56:07 -0800

    updates intro to site.description

--- site/snippets/intro.php
+++ site/snippets/intro.php
@@ -1,9 +1,5 @@
 <div class="c-intro">
   <p>
-    I'm a Designer based in San Francisco, California.
-  </p>
-  <p>
-    I help deliver quality products through research and design sprints, user-centered interaction design,
-    rapid prototyping, and polished front-end design. 
+  <?= $site->description() ?>
   </p>
 </div>
