80a26743faffb4a9262a24ac5747a72e63ae535c
Colin Johnston <dev@electronspin.com>
2020-02-28 15:12:31 -0800

    testing kirby3-srcset plugin

---
+++ site/plugins/kirby3-srcset/tests/site/config/config.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'debug' => true,
+    'thumbs' => [
+        'quality' => 42,
+        'srcsets' => [
+            'default' => [320, 1200],
+            'breakpoints' => [576, 768, 992, 1200],
+        ],
+    ],
+];
