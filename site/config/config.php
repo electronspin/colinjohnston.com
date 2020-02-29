9ce8dd845d15cc1184f4e22a1590889824dbff9c
Colin Johnston <dev@electronspin.com>
2020-02-29 12:51:05 -0800

    adds projects preset for srcset experiments - see plugin jd-k-srcset

--- site/config/config.php
+++ site/config/config.php
@@ -3,8 +3,14 @@
 return [
   'debug' => true,
   'url' => 'http://colinjohnston.local',
-  //'url' => 'http://localhost:3000',
   'markdown' => [
     'extra' => true
-  ]
+  ],
+  'thumbs' => [
+    'srcsets' => [
+      'default' => [400, 800, 1200, 1600],
+      'projects' => [800, 1200, 2000],
+      'cover' => [800, 1024, 2048],
+    ],
+  ],
 ];
