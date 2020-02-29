8290b48b7ea5eb59387d81153453fb3ca5e4d935
Colin Johnston <dev@electronspin.com>
2020-02-29 13:26:24 -0800

    cleans up plugin source

--- site/plugins/jd-k-srcset/index.php
+++ site/plugins/jd-k-srcset/index.php
@@ -13,16 +13,17 @@ Kirby::plugin('colin-johnston/jd-k-srcset', [
             'html' => function($tag) use ($originalTag)  {
 
                 $file = $tag->file($tag->value());
-                // $presets = $kirby->option('thumbs.srcsets.default');
+
+                // gets srcset array from config
                 $presets = option('thumbs.srcsets.default');
-                dump($presets);
+
+                // sets srcset as kirbytag array with fallback to config array
                 $srcset = $tag->srcset ? explode(',', $tag->srcset): $presets;
-                dump($srcset);
                 
-                // from texnixie 
+                // from @texnixe - casts kirbytext array strings to ints
                 $srcset = array_map(function($item) {
                     return (int) $item;
-                  }, $srcset);
+                }, $srcset);
 
                 $result = $originalTag['html']($tag);
 
@@ -30,49 +31,22 @@ Kirby::plugin('colin-johnston/jd-k-srcset', [
                     return $result;
                 }
 
-                // $files = [];
-                // foreach ($srcset as $width) {
-                //     // if ($file->width() < $width) {
-                //     //     continue;
-                //     // }
-
-                //     $imageResized = $file->resize($width);
-                //     $files[] = $imageResized->url();
-                //     // $srcset[] = $imageResized->url() . ' ' . $imageResized->width() . 'w';
-                // }
-                // dump($files);
-            
-
                 $pattern = '/<img.*?>/i';
 
-                // build a new image tag with the srcset
-                $image = Html::img($tag->src, [
-                    'width'  => $tag->width,
-                    'height' => $tag->height,
-                    'class'  => $tag->imgclass,
-                    'title'  => $tag->title,
-                    'alt'    => $tag->alt ?? ' ',
-                    'srcset' => $file->srcset($srcset),
-                ]);
-                dump($image);
-             
-                // resize
-                // $widths = $kirby->option('thumbs.srcsets.default', $srcset);
+            // build a new image tag with the srcset
+            $image = Html::img($tag->src, [
+                'width' => $tag->width,
+                'height' => $tag->height,
+                'class' => $tag->imgclass,
+                'title' => $tag->title,
+                'alt' => $tag->alt ?? ' ',
+                'srcset' => $file->srcset($srcset),
+            ]);
 
-                // foreach ($widths as $width) {
-                //     if ($image->width() < $width) {
-                //         continue;
-                //     }
+            // replace the old image tag
+            $result = preg_replace($pattern, $image , $result);
 
-                //     $imageResized = $image->resize($width);
-                //     $srcset[] = $imageResized->url() . ' ' . $imageResized->width() . 'w';
-                // }
-
-
-                // replace the old image tag
-                $result = preg_replace($pattern, $image , $result);
-
-                return $result;
+            return $result;
             }
         ]
     ]
