503270941c8a313fa1917e9255e485b9a0b108a7
Colin Johnston <dev@electronspin.com>
2020-05-06 15:30:31 -0700

    updates to jd-k-srcset plugin

--- site/plugins/jd-k-srcset/index.php
+++ site/plugins/jd-k-srcset/index.php
@@ -43,6 +43,21 @@ Kirby::plugin('colin-johnston/jd-k-srcset', [
                     $srcsettag = 'data-srcset';
                 }
 
+                dump($srctag);
+                dump($srcsettag);
+
+                // if srctag = data-src then grab this regex and strip it out
+                // \sset\s*=(["'])(?:(?=(\\?))\2.)*?\1
+                // much faster: \ssrc\s*=\s*"(.+?)"
+                // if srcsettag = data-srcset then grab this regex and strip it out 
+                // \ssrcset\s*=(["'])(?:(?=(\\?))\2.)*?\1
+                // much faster: \ssrcset\s*=\s*"(.+?)"
+                // source and explanation at https://regex101.com/r/Usz15o/1 and https://regex101.com/r/Usz15o/2
+                // pattern for replace https://github.com/bnomei/kirby3-srcset/blob/f82bcf841a05097b31b7ad851206ee5114e5f4a3/index.php#L79
+
+
+
+
                 // build a new image tag with the srcset 
                 $image = Html::img($tag->src, [
                     'width' => $tag->width,
@@ -52,16 +67,18 @@ Kirby::plugin('colin-johnston/jd-k-srcset', [
                     'alt' => $tag->alt ?? ' ',
                     // 'srcset' => $file->srcset($srcset), [see note 1]
                     // the following are ignored if they duplicate src and scrset, added if not
-                    // there's got to be ab tter way to do this...
-                    strval($srctag) => $tag->src,
+                    // there's got to be a better way to do this...
                     strval($srcsettag) => $file->srcset($srcset),
+                    strval($srctag) => $tag->src,
                     // [note 1] for some reason if set of data-src and data-srcset are generated, 
                     // I also get a `srcset` even though I'm not explicitily generating it. Confused.
                 ]);
 
                 // replace the old image tag
-                $result = preg_replace($pattern, $image , $result);
+                $result = preg_replace($pattern, $image, $result);
 
+                dump($image);
+                
                 return $result;
             }
         ]
