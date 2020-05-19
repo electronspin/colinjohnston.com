30fd958be1eb7d3be3a9a54d0fcc14a76ff7f138
Colin Johnston <dev@electronspin.com>
2020-05-18 19:02:41 -0700

    fixes active class on main pages menu

--- site/snippets/menu-pages.php
+++ site/snippets/menu-pages.php
@@ -4,14 +4,16 @@
 $items = $pages->listed();
 
 // only show the menu if items are available
-if($items->isNotEmpty()):
+if($items->isNotEmpty() ):
 
 ?>
+
+<!-- ->depth() < 2) -->
 
 <li class="c-side-nav__title">&rarr;</li>
 
 <?php foreach($items as $item): ?>
-  <li <?php e($item->isOpen(), 'class="c-side-nav__item active"') ?> class="c-side-nav__item"><a href="<?= $item->url() ?>"><?= $item->title()->html() ?></a></li>
+  <li <?php e($item->isActive(), 'class="c-side-nav__item active"') ?> class="c-side-nav__item"><a href="<?= $item->url() ?>"><?= $item->title()->html() ?></a></li>
 <?php endforeach ?>
 
 <?php endif ?>
