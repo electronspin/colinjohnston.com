14991cf57812a23b9bd01b3b3a1c8bf64e7adaa7
Colin Johnston <dev@colinjohnston.com>
2020-09-16 18:46:10 -0700

    add fathom snippet

--- site/snippets/head.php
+++ site/snippets/head.php
@@ -1,13 +1,14 @@
 <!DOCTYPE html>
 <html>
 <head>
+
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="/favicon.ico" rel="icon" type="image/x-icon">
   <meta name="description" content="<?= $site->description() ?>">
   
   <?php if ($page->isHomePage()): ?>
-  <title>Colin Johnston  - Product Design, Visual Design, UX Strategy, Brand Development - San Francisco Web Design</title>
+  <title>Colin Johnston - Product Design, Visual Design, UX Strategy, Brand Development - San Francisco Web Design</title>
   <?php else: ?>
   <title><?= $page->title() . ' – ' . $site->title() ?></title>
   <?php endif ?>
@@ -19,5 +20,7 @@
   <script src="/assets/js/halkaBox.min.js"></script>
   <script src="https://code.jquery.com/jquery-1.9.1.min.js" integrity="sha256-wS9gmOZBqsqWxgIVgA8Y9WcQOa7PgSIX+rPA0VL2rbQ=" crossorigin="anonymous"></script>
   <script src="/assets/js/app.min.js"></script>
+  <script src="https://cdn.usefathom.com/script.js" site="XRUIYLTW" defer></script>
+
 </head>
 <body>
