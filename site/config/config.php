0339ed11504c66aa96ac4ae4ffefb85c1bd81ba4
Colin Johnston <dev@colinjohnston.com>
2020-10-16 21:18:49 -0700

    configures meta-tags plugin - requires text field for all pages except home

--- site/config/config.php
+++ site/config/config.php
@@ -4,7 +4,46 @@ return [
   'debug' => true,
   'bnomei.dotenv.dir' => function () {
     return realpath(kirby()->roots()->index() . '/../');
-},
+  },
+  'pedroborges.meta-tags.default' => function ($page, $site) {
+    return [
+      // 'title' => $site->title(),
+      'meta' => [
+        'description' => $site->description()
+      ],
+      'link' => [
+        'canonical' => $page->url()
+      ],
+      'og' => [
+        'title' => $page->isHomePage()
+          ? $site->title()
+          : $page->title(),
+        'type' => 'website',
+        'site_name' => $site->title(),
+        'url' => $page->url()
+      ],
+      'twitter' => [
+        'card' => 'summary',
+        'site' => $site->twitter(),
+        'title' => $page->title(),
+        'description' => $page->isHomePage()
+          ? $site->description()
+          : $page->text()->excerpt(128),
+        'namespace:image' => function ($page) {
+          if ($image = $page->images()->findBy('template', 'cover'))
+            return [
+              'image' => $image->url(),
+              'alt' => $image->alt()
+            ];
+          else
+            return [
+              'image' => '',
+              'alt' => ''
+            ];
+        }
+      ]
+    ];
+  },
   // 'url' => 'http://colinjohnston.local',
   'markdown' => [
     'extra' => true
