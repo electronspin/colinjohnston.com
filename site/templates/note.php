86a62825945114000e861f7dbb07389a072d8a37
= <dev@colinjohnston.com>
2021-03-13 13:02:01 -0800

    fixes body content block width in note template

--- site/templates/note.php
+++ site/templates/note.php
@@ -7,11 +7,15 @@
 
     <main id="content" class="o-content o-layout__grid-item c-page">
 
+    <div class="c-page__body">
+
     <h1 class="c-page__title"><?= $page->title() ?></h1>
 
     <p class="c-page__date"><?= $page->published()->toDate('j F Y') ?></p>
 
     <?= $page->text()->kirbytext() ?>
+
+    </div>
 
     </main>
 
