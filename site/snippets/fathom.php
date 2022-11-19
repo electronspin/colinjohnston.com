c77d2c4f8823ab711d2aee6cb0b24650d9baf268
= <dev@colinjohnston.com>
2022-11-18 17:37:24 -0800

    updates fathom snippet to new usage

--- site/snippets/fathom.php
+++ site/snippets/fathom.php
@@ -2,5 +2,5 @@
 $kirbyenv = env('APP_MODE');  
 $fathom_id = env('FATHOM_ID');
 if ($kirbyenv === 'production'): ?>
-<script src="https://cdn.usefathom.com/script.js" site="<?php echo $fathom_id; ?>" defer></script>
+<script src="https://invisible-ninja.colinjohnston.com/script.js" data-site="<?php echo $fathom_id; ?>" defer></script>
 <?php endif ?>
